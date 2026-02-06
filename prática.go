ba7dd52058fa113522389b9cdbb8f11b192bf49e
varyn7 <joaojaimesampaio7@gmail.com>
2026-02-06 12:14:57 -0300

    Add files via upload
    exercicio de comando de formatação

---
+++ prática.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "olá "
+	b := "bom dia, "
+	c := "como você está?, "
+	d := "  resposta: vou bem "
+
+	fmt.Printf("%s %s %s %s \n", a, b, c, d)
+
+}
