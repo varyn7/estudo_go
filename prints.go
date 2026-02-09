7ce381d831639f801c7480bef8a4ec262f23b86e
varyn7 <joaojaimesampaio7@gmail.com>
2026-02-09 10:59:45 -0300

    Add files via upload
    estudo das diferentes funções de print como println e print convencional entre outros assim como o de conversão (Sprint) que no caso converte para string

---
+++ prints.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("mesma")
+	fmt.Println("linha, ")
+
+	fmt.Print("nova ")
+	fmt.Print("linha ")
+	x := 3.14
+	fmt.Sprint(x)
+	fmt.Println("o valor de x é,", x)
+
+}
