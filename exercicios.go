c26638c8e35f35886b125fcf1e7f80305a6a7801
varyn7 <joaojaimesampaio7@gmail.com>
2026-02-13 09:56:32 -0300

    Add files via upload
    mais alguns exercicios básicos de golang (enunciado explicativo)

--- exercicios.go
+++ exercicios.go
@@ -1,10 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// primeiro exercicio multiplicar dois numeros inteiros
 func main() {
-	// Declarando variáveis
-	a := 2
-	b := 2
-	fmt.Println(a + b)
+	a := 3
+	b := 7
+	fmt.Println(a * b)
+
+	// segundo exercicio somar dois numeros
+	c := 2.5
+	d := 4.7
+	fmt.Println(d + c)
+
+	// terceiro exercicio subtrair dois numeros
+	e := 4
+	f := 6
+	fmt.Println(f - e)
+
+	// quarto exercicio calculo de divisao
+	g := 2
+	h := 2
+	fmt.Println(g / h)
+	//exercicio definindo um numero par ou impar
+	var numero int
+
+	fmt.Print("Digite um número: ")
+	fmt.Scan(&numero)
+
+	if numero%2 == 0 {
+		fmt.Println("O número é PAR")
+	} else {
+		fmt.Println("O número é ÍMPAR")
+	}
+	//reconhecendo o nome de uma pessoa
+	var nome string
+	fmt.Print("Digite nome: ")
+	fmt.Scan(&nome)
+	if nome == "john" {
+		fmt.Println("o nome é massa  ")
+	} else {
+		fmt.Println("é muito diferente ")
+
+	}
+
 }
