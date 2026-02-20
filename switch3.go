028a640c7c1d1d3ffda446d2662b0e37c56401c2
varyn7 <joaojaimesampaio7@gmail.com>
2026-02-20 10:40:13 -0300

    Add files via upload
    aprofundamento da função switch e seleção de parâmetros dentro da função

---
+++ switch3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func tipo(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "inteiro"
+	case float32, float64:
+		return "real"
+	case string:
+		return "string"
+	case func():
+		return "func"
+	case bool:
+		return "bool"
+	default:
+		return "desconhecido"
+	}
+}
+
+func main() {
+	fmt.Println(tipo(10))
+	fmt.Println(tipo(3.14))
+	fmt.Println(tipo("oi"))
+	fmt.Println(tipo(func() {}))
+	fmt.Println(tipo(time.Now()))
+}
